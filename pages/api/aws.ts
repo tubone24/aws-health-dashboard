6dd460ad5e3e19a559851499b71eb2c572a3af7e
tubone24 <tubo.yyyuuu@gmail.com>
2021-01-13 00:05:04 +0900

    CI

--- pages/api/aws.ts
+++ pages/api/aws.ts
@@ -18,33 +18,33 @@ interface AwsStatusArchive {
 }
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  axios.get<AwsStatusResp>(
-    'https://status.aws.amazon.com/data.json1'
-  ).then(resp => {
-    const handlerResp = resp.data.archive.map((x) => ({
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      service_name: x.service_name,
-      summary: x.summary,
-      region: x.service.split('-').slice(1).join('-') || 'Global',
-      date: x.date,
-      status: x.status,
-      details: x.details,
-      service: x.service.split('-')[0],
-      description: x.description
+  axios
+    .get<AwsStatusResp>('https://status.aws.amazon.com/data.json1')
+    .then((resp) => {
+      const handlerResp = resp.data.archive.map((x) => ({
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        service_name: x.service_name,
+        summary: x.summary,
+        region: x.service.split('-').slice(1).join('-') || 'Global',
+        date: x.date,
+        status: x.status,
+        details: x.details,
+        service: x.service.split('-')[0],
+        description: x.description
           .replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')
           .replace(/&nbsp;/g, '\n'),
-    }))
-    res.statusCode = 200
-    // eslint-disable-next-line no-console
-    console.log(handlerResp)
-    res.json(handlerResp)
-  }).catch(error => {
-    console.error(error.response)
-    res.statusCode = error.response.status || 500
-    res.statusMessage = error.response.statusText || "InternalServerError"
-    res.json({error: error.response.statusText || "InternalServerError"})
-  })
+      }))
+      res.statusCode = 200
+      // eslint-disable-next-line no-console
+      console.log(handlerResp)
+      res.json(handlerResp)
+    })
+    .catch((error) => {
+      console.error(error.response)
+      res.statusCode = error.response.status || 500
+      res.statusMessage = error.response.statusText || 'InternalServerError'
+      res.json({ error: error.response.statusText || 'InternalServerError' })
+    })
 }
-
 
 export default handler
